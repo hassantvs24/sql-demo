d645e7edbd505b3c97dd8bdde5c9a385247daef4
Hassan Hussain <49582324+hassantvs24@users.noreply.github.com>
2020-03-15 19:06:34 +0600

    Allow Origin

--- startup/routes.js
+++ startup/routes.js
@@ -9,6 +9,13 @@ const auth = require('../routes/auth');
 const users = require('../routes/users');
 
 module.exports = function (app){
+
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+      });
+
     app.use(express.json()); // for parsing application/json
     app.use(express.static('public'));//Access public asset
     app.use(error);
